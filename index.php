e8ef2a9243be618ab7b4f442218de6fd52a8e470
Sunday Omolewu <omolewu12@gmail.com>
2023-07-17 16:04:33 +0100

    day one

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="<?php ?>">
+<head>
+    <meta  charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction to PHP</title>
+</head>
+<body>
+
+<?php
+$first_name="Sunday";
+$last_name="Omolewu <br>";
+echo $first_name . " ". $last_name;
+$a=34;
+$b= 34.56;
+$amount =null;
+ echo var_dump($amount)
+?>
+
+
+    <?php 
+   echo  "Hello PHP"
+    ?>
+    <h2><?php echo "Welcome to PHP Class" ?></h2>
+</body>
+</html>
